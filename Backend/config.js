5936831f67522b3653b2d06d1c273862d25e695c
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-06-09 13:45:01 +0545

    push

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const MONGODB_URI = "mongodb+srv://root:Sardarabhishek@book-store-mern.tm1j5cl.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Book-Store-MERN";
--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";  // You need to import mongoose
+import { PORT, MONGODB_URI } from "./config.js";
 
 const app = express();
 
-app.get('/', (request, response)=>{
+app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome to the Book Store");
 });
 
-app.listen(PORT, () =>{
-    console.log(`Server running on port ${PORT}`)
-})
+// Connect to MongoDB and start server
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    });
