d3c70ff478ac3bf4c02741eaab411f2682bc317e
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-06-10 13:06:14 +0545

    push

--- Backend/index.js
+++ Backend/index.js
@@ -1,34 +1,76 @@
-import express from "express";
-import mongoose from "mongoose";  // You need to import mongoose
+import express, { request } from "express";
+import mongoose from "mongoose"; // You need to import mongoose
 import { PORT, MONGODB_URI } from "./config.js";
-import {Book} from './models/bookModel.js';
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(234).send("Welcome to the Book Store");
+//middleware for parsing request body
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to the Book Store");
 });
 
 //route for save a new book
 
-app.post('/books', async (request, response) =>{
-    try{
-        if(
-            !request.body.title ||
-            
-        )
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear ||
+      !request.body.price
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
     }
-})
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+      price: request.body.price,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+//Route for Get all books from database
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+
+    return response.status(200).json({
+        count:books.length,
+        data: books,
+    })
+
+    response.status(200).json(books);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 // Connect to MongoDB and start server
-mongoose.connect(MONGODB_URI)
-    .then(() => {
-        console.log('App connected to database');
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        })
-    })
-    .catch((error) => {
-        console.log(error);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
